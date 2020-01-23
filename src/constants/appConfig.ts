01a344cbcb93441ad8bd423e0a994b2d94eae435
Jigar Prajapati <jigar.prajapati@tatvasoft.com>
2020-01-23 15:39:40 +0530

    Dynamic rest API with persist data

--- src/constants/appConfig.ts
+++ src/constants/appConfig.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "https://my-json-server.typicode.com/tatva-denish/json-rest-api";
 export const DATE_FORMATE = "YYYY-MM-DD";
 export const DEFAULT_LANG =  localStorage.getItem('lng') ?  localStorage.getItem('lng') : 'en';
